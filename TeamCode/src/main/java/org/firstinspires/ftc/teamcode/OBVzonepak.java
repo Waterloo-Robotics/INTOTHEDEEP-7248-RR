4d4db458293f33cfd057098f1be65187e503b030
Waterloo Robotics <ftcteam7247@gmail.com>
2025-01-24 16:25:37 -0600

    one sample works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OBVzonepak.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OBVzonepak.java
@@ -8,11 +8,17 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.arcrobotics.ftclib.controller.PController;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -82,6 +88,58 @@ public class OBVzonepak extends LinearOpMode {
         }
     }
 
+    public class ScoringSlides{
+        private DcMotorEx rightSlide;
+        private DcMotorEx leftSlide;
+
+        private PController controller;
+
+        public ScoringSlides(HardwareMap hardwareMap){
+            rightSlide = (DcMotorEx) hardwareMap.get(DcMotor.class, "R_slide");
+            rightSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+            rightSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            leftSlide = (DcMotorEx) hardwareMap.get(DcMotor.class, "L_slide");
+            leftSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+            rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            rightSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            controller=new PController(.002);
+            controller.setTolerance(35);
+
+        }
+
+//        public class Home implements Action {
+//            @Override
+//            public boolean run(@NonNull TelemetryPacket packet) {
+//                leftSlide.setPosition(Constants.SCORING_ARM_HOME);
+//                rightSlide.setPosition(1 - Constants.SCORING_ARM_HOME);
+//                return false;
+//            }
+//        }
+//        public Action home() {
+//            return new Home();
+//        }
+
+        public class Basket implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+
+                controller.setSetPoint(Constants.SLIDE_BASKET);
+                double power = controller.calculate(leftSlide.getCurrentPosition());
+                leftSlide.setPower(power);
+                rightSlide.setPower(power);
+
+                return !controller.atSetPoint();
+            }
+        }
+        public Action basket() {
+            return new Basket();
+        }
+    }
+
 
     @Override
 public void runOpMode(){
@@ -92,11 +150,16 @@ public void runOpMode(){
         MecanumDrive drive = new MecanumDrive(hardwareMap, startpose);
         ScoringClaw scoringClaw = new ScoringClaw(hardwareMap);
         ScoringArm scoringArm = new ScoringArm(hardwareMap);
+        ScoringSlides scoringslides = new ScoringSlides(hardwareMap);
 
         TrajectoryActionBuilder park = drive.actionBuilder(startpose)
-                .strafeTo(new Vector2d(-54, -54))
+                .strafeTo(new Vector2d(-50, -58))
                 .waitSeconds(0.1)
-                .turnTo(Math.toRadians(45));
+                .turnTo(Math.toRadians(45))
+                .waitSeconds(1);
+
+        TrajectoryActionBuilder safe = park.endTrajectory().fresh()
+                .lineToY(-50);
 
         Actions.runBlocking(new ParallelAction(
                 scoringClaw.close(),
@@ -109,7 +172,16 @@ public void runOpMode(){
 
         Actions.runBlocking(new ParallelAction(
                 park.build(),
-                scoringArm.basket()
+                scoringArm.basket(),
+                scoringslides.basket()
+        ));
+
+        Actions.runBlocking(new SequentialAction(
+                scoringClaw.open(),
+                new SleepAction(1),
+                scoringClaw.close(),
+                scoringArm.home(),
+                safe.build()
         ));
 
     }
